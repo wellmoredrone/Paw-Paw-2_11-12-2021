f0f461b10372811f64d0f978fec2cade116d9a66
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2021-11-15 09:34:55 -0500

    init

---
+++ data.js
@@ -0,0 +1,50 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-paw-paw-2-11-12-2021",
+      "name": "Paw Paw 2 11-12-2021",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.01685493918059855,
+        "pitch": 0.42420746297083056,
+        "fov": 1.014167193592228
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Paw Paw 2 11-12-2021",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
